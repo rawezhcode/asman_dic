df1c0f4be429cf042a4981928458bc9921a96e2f
RawezhCST <rawezh180@gmail.com>
2022-04-21 13:28:32 +0300

    r

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:3333/google/callback',
+        'redirect' => 'http://phplaravel-762476-2581598.cloudwaysapps.com/google/callback',
     ],
 
 ];
