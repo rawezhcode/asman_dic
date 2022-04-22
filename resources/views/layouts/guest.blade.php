ef1aaecfe60486a05f9c8dba4cf2c98eed8c95f6
RawezhCST <rawezh180@gmail.com>
2022-04-22 16:57:23 +0300

    r

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Asman Dictionary</title>
+        <link rel="icon" type="image/png" href="{{ asset('assets/img/asman2.png') }}">
+        <meta name="title" content="Asman Dictionary">
+        <meta name="description" content="Asman Dictionary">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:title" content="Asman Dictionary" />
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ url()->current() }}" />
+        <!-- Twitter -->
+        <meta property="twitter:description" content="Asman Dictionary">
+        <meta property="twitter:url" content="{{ url()->current() }}">
+        <meta property="twitter:title" content="Asman Dictionary">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
